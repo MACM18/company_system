92ef65f3a7db9723eae24ba2fdf398d0fb8dd7c8
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2024-02-19 18:15:52 -0800

    add files and update files

---
+++ dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$servername='localhost';
+$username='root';
+$password='';
+$dbname='company system';
+$con=mysqli_connect($servername,$username,$password,$dbname)or die ("db connection failed");
+?>
